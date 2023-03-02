972de4dfc212e2ac511843b5e0c1e765a77b8d8f
pjpawel <pawel.jan.podgorski@gmail.com>
2023-03-02 19:32:33 +0100

    Change Kernel loading without Env

--- public/index.php
+++ public/index.php
@@ -2,9 +2,8 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use pjpawel\LightApi\Env;
 use pjpawel\LightApi\Kernel;
 use pjpawel\LightApi\Component\Runner\HttpRunner;
 
-$runner = new HttpRunner(new Kernel(Env::getConfigFromEnv(__DIR__ . '/../config/')));
+$runner = new HttpRunner(new Kernel(__DIR__ . '/../config/'));
 $runner->run();
